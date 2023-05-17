9fb1f3bf7d67578fb89f0b5152e6a83c1ccdc36a
Taflnne15 <taflynnepetersen@gmail.com>
2023-05-17 10:22:45 +0200

    day4 commit 1

---
+++ script/code.js
@@ -0,0 +1,18 @@
+// question 1
+let person1 = {
+firstname: 'Taflynne Petersen',
+age: 18,
+gender: 'female'  
+};
+console.log(typeof person1)
+// question 2
+let person2 = {
+firstname: 'Jayde Lewis',
+age: 24,
+gender: 'male'
+};
+console.log(typeof person2)
+// question 3 
+let person = [ person1, person2]
+console.log(person);
+
